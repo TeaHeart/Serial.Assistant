328318ef58c962edc1a9b304067696c4e8cc8c9f
秉辰 韩 <1069696634@qq.com>
2024-04-27 23:51:16 +0800

    新增 添加所有

---
+++ Serial.Assistant/Assert.cs
@@ -0,0 +1,17 @@
+﻿namespace Serial.Assistant;
+
+public static class Assert {
+    public static void RequireTrue(bool expression) {
+        if (!expression) {
+            throw new ArgumentException(null, nameof(expression));
+        }
+    }
+
+    public static void RequireAscend(params int[] args) {
+        for (var i = 1; i < args.Length; i++) {
+            if (args[i - 1] > args[i]) {
+                throw new ArgumentOutOfRangeException(nameof(args), args[i - 1], null);
+            }
+        }
+    }
+}
---
+++ Serial.Assistant/EnumerableHelper.cs
@@ -0,0 +1,23 @@
+﻿namespace Serial.Assistant;
+
+using System.Collections;
+
+public static class EnumerableHelper {
+    public static string ToString<T>(this IEnumerable @this, string separator = ", ", string prefix = "[", string suffix = "]") {
+        return ToString(@this.Cast<T>(), separator, prefix, suffix);
+    }
+
+    public static string ToString<T>(this IEnumerable<T> @this, string separator = ", ", string prefix = "[", string suffix = "]") {
+        return prefix + string.Join(separator, @this) + suffix;
+    }
+
+    public static void ForEach<T>(this IEnumerable @this, Action<T> action) {
+        ForEach(@this.Cast<T>(), action);
+    }
+
+    public static void ForEach<T>(this IEnumerable<T> @this, Action<T> action) {
+        foreach (var item in @this) {
+            action(item);
+        }
+    }
+}
